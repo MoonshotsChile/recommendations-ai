14e4252142a73680657701f93f7f20fe7daa6a14
Rodrigo Riquelme <rodrigo.riquelme.e@outlook.com>
2021-03-11 16:44:21 -0300

    random benefit

--- src/domain/BenefitsUseCase.ts
+++ src/domain/BenefitsUseCase.ts
@@ -1,11 +1,18 @@
 import { API_URL } from "../config/constants";
 
 export class BenefitsUseCase {
+    BENEFITS_NUM = 500
+
     list() {
         return fetch(`${API_URL}/benefits`)
     }
 
     find(id: number) {
         return fetch(`${API_URL}/benefits/${id}`)
+    }
+
+    random() {
+        const index = Math.floor(Math.random() * this.BENEFITS_NUM)
+        return fetch(`${API_URL}/benefits?_start=${(index - 1)}&_end=${index}`)
     }
 }
