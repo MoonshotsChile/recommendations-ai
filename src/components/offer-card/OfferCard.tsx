f27458cfdbcee9d1983d10e172f6f15488912e61
Rodrigo Riquelme <rodrigo.riquelme.e@outlook.com>
2021-03-11 16:19:20 -0300

    api promotions

--- src/components/offer-card/OfferCard.tsx
+++ src/components/offer-card/OfferCard.tsx
@@ -3,15 +3,27 @@ import React, { FC } from "react";
 const OfferCard: FC = () => {
     return (
         <div className="card">
-            <div className="card-header">
-                <h5>FUU</h5>
-            </div>
             <div className="card-content">
-                <h2>Wii</h2>
+                <p className="title">
+                    “There are two hard things in computer science: cache invalidation, naming things, and off-by-one
+                    errors.”
+                </p>
+                <p className="subtitle">
+                    Jeff Atwood
+                </p>
             </div>
-            <div className="card-footer">
-                <p>Footer</p>
-            </div>
+            <footer className="card-footer">
+                <p className="card-footer-item">
+                  <span>
+                    View on <a href="https://twitter.com/codinghorror/status/506010907021828096">Twitter</a>
+                  </span>
+                            </p>
+                            <p className="card-footer-item">
+                  <span>
+                    Share on <a href="#">Facebook</a>
+                  </span>
+                </p>
+            </footer>
         </div>
     )
 }
