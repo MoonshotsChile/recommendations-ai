7761436bc5af4a5d68aaf81d7859569287dfba6b
Rodrigo Riquelme <rodrigo.riquelme.e@outlook.com>
2021-03-11 14:16:11 -0300

    icons

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,8 +1,12 @@
-export { default as appleIcon } from './icons/apple.svg';
 export { default as twitterIcon } from './icons/twitter.svg';
 export { default as facebookIcon } from './icons/facebook-f.svg';
 export { default as instagramIcon } from './icons/instagram.svg';
 export { default as sBankIcon } from './icons/sbank.svg';
-export { default as movingAwayIcon } from './icons/moving-away.svg';
 export { default as moonshotsLogoIcon } from './icons/moonshots-logo.svg';
+export { default as tinderButtonLaterIcon } from './icons/tinder-button-later.svg';
+export { default as tinderButtonLikeIcon } from './icons/tinder-button-like.svg';
+export { default as tinderButtonNoIcon } from './icons/tinder-button-no.svg';
+export { default as tinderButtonNoDisabledIcon } from './icons/tinder-button-no-disabled.svg';
+export { default as tinderButtonFireIcon } from './icons/tinder-fire.svg';
+
 
