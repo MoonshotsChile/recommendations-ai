611597f61845829f1eaefdc1d5c0e6800185dedd
Rodrigo Riquelme <rodrigo.riquelme.e@outlook.com>
2021-03-11 15:39:51 -0300

    using component tinder card

--- src/pages/OfferPage.tsx
+++ src/pages/OfferPage.tsx
@@ -1,9 +1,18 @@
 import React, { FC, useEffect } from 'react';
 import OfferCard from "../components/offer-card/OfferCard";
 import { BenefitsUseCase } from "../domain/BenefitsUseCase";
+import TinderCard from "react-tinder-card";
 
 const OfferPage: FC = () => {
     const useCase = new BenefitsUseCase()
+
+    const onSwipe = (direction: string) => {
+        console.log(`You swiped: ${direction}`)
+    }
+
+    const onCardLeftScreen = (myIdentifier: string) => {
+        console.log(`${myIdentifier} left the screen`)
+    }
 
     useEffect(() => {
         useCase.list().then((data: Response)=> console.log(data.json()))
@@ -11,7 +20,9 @@ const OfferPage: FC = () => {
 
     return (
         <div className="section">
-            <OfferCard/>
+            <TinderCard  onSwipe={onSwipe} onCardLeftScreen={() => onCardLeftScreen('fooBar')}>
+                <OfferCard/>
+            </TinderCard>
         </div>
     );
 }
