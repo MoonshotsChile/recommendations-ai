e0ec3583240617df5bf518d153e05ccee815f010
Rodrigo Riquelme <rodrigo.riquelme.e@outlook.com>
2021-03-22 12:12:46 -0300

    fixes mockserver

--- src/config/constants.tsx
+++ src/config/constants.tsx
@@ -1,2 +1,2 @@
 export const API_URL =
-    process.env.REACT_APP_API_URL ? process.env.REACT_APP_API_URL : 'https://api.moonshots.cl'
+    process.env.REACT_APP_API_URL ? process.env.REACT_APP_API_URL : 'https://api.moonshots.cl/benefits'
