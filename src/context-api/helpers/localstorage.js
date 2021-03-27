6d153a0953f662f87c1f5c504ed97e8e7dcfb5d5
Rodrigo Riquelme <rodrigo.riquelme.e@outlook.com>
2021-03-26 22:34:59 -0300

    removed unnecesary warning

--- src/context-api/helpers/localstorage.js
+++ src/context-api/helpers/localstorage.js
@@ -3,7 +3,6 @@ export const setLocalStorage = (key, value) => {
   try {
     window.localStorage.setItem(key, JSON.stringify(value));
   } catch (e) {
-    console.warn(e)
     window.localStorage.setItem(key, value)
   }
 }
@@ -13,7 +12,6 @@ export const getLocalStorage = (key) => {
   try {
     return JSON.parse(value)
   } catch (e) {
-    console.warn(e)
     return value;
   }
 }
