6dd15bc9c36b153ad5da46a81d8c95050101e821
Rodrigo Riquelme <rodrigo.riquelme.e@outlook.com>
2021-03-29 17:15:06 -0300

    /missions endpoint

--- src/domain/MisionsUseCase.ts
+++ src/domain/MisionsUseCase.ts
@@ -4,25 +4,25 @@ export class MisionsUseCase {
     MISIONS_NUM = 500
 
     list(queryParams = ''): Promise<Response>  {
-        return fetch(`${API_URL}/misions/${queryParams}`)
+        return fetch(`${API_URL}/missions/${queryParams}`)
     }
 
     find(id: number): Promise<Response> {
-        return fetch(`${API_URL}/misions/${id}`)
+        return fetch(`${API_URL}/missions/${id}`)
     }
 
     random(): Promise<Response>  {
         const index = Math.floor(Math.random() * this.MISIONS_NUM)
-        return fetch(`${API_URL}/misions/?_start=${(index - 1)}&_end=${index}`)
+        return fetch(`${API_URL}/missions/?_start=${(index - 1)}&_end=${index}`)
     }
 
     randomStack(length = 1): Promise<Response>  {
         const index = Math.floor(Math.random() * this.MISIONS_NUM - 1) + 1
-        return fetch(`${API_URL}/misions/?_start=${(index - length)}&_end=${index}`)
+        return fetch(`${API_URL}/missions/?_start=${(index - length)}&_end=${index}`)
     }
 
 
     custom(start: number, end: number): Promise<Response>  {
-        return fetch(`${API_URL}/misions/?_start=${(start - 1)}&_end=${end}`)
+        return fetch(`${API_URL}/missions/?_start=${(start - 1)}&_end=${end}`)
     }
 }
